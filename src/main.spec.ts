93c74def431b88bb13585d75ad9250a177889cff
Evgeny Glukhov <evglukhov@luxoft.com>
2018-10-13 20:25:47 +0300

    add tests to search term fn

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,65 +1,147 @@
-import { cold, getTestScheduler } from 'jasmine-marbles';
-import * as main from './main';
+import { cold, getTestScheduler } from 'jasmine-marbles'
+import * as main from './main'
+import { TestScheduler } from 'rxjs/testing';
 
-type Values = {
-    [index: string]: string
-};
+type Values<V> = {
+  [index: string]: V
+}
 
 const helpers = {
-    generateSymbols(): string[] {
-        return new Array( 26 ).fill('').map((_: string, i: number) => String.fromCharCode( 65 + i ).toLowerCase());
-    },
-    generateAlphabet() {
-        this.generateSymbols()
-            .reduce((acc: Values, char: string) => {
-                acc[char] = char;
+  generateSymbols(): string[] {
+    return new Array(26)
+      .fill('')
+      .map((_: string, i: number) => String.fromCharCode(65 + i).toLowerCase())
+  },
+  generateAlphabet() {
+    return this.generateSymbols().reduce((acc: Values<string>, char: string) => {
+      acc[char] = char
 
-                return acc;
-            }, {});
-    }
+      return acc
+    }, {})
+  },
+  generateEvents(type: string) {
+    return this.generateSymbols().reduce((acc: Values<object>, char: string) => {
+      acc[char] = {
+        type,
+        key: char,
+        which: char.charCodeAt(0)
+      };
+
+      return acc;
+    }, {})
+  }
 }
 
 describe('#sum()', () => {
-    it('should sum numbers correctly', () => {
-        const numbers$ = cold('--a--b--', { a: 1, b: 2 });
-        const expected$ = cold('--c--d--', { c: 1, d: 3 });
+  it('should sum numbers correctly', () => {
+    const numbers$ =  cold('--a--b--', { a: 1, b: 2 })
+    const expected$ = cold('--c--d--', { c: 1, d: 3 })
 
-        expect(main.sum(numbers$)).toBeObservable(expected$);
-    })
-});
+    expect(main.sum(numbers$)).toBeObservable(expected$)
+  })
+})
 
 describe('#delayedSum()', () => {
-    it('should sum numbers with delay correctly', () => {
-        const scheduler = getTestScheduler();
-        const numbers$ = cold('--a---b---', { a: 1, b: 2 });
-        const ms = 10;
+  it('should sum numbers with delay correctly', () => {
+    const scheduler = getTestScheduler()
+    const numbers$ =  cold('--a---b---', { a: 1, b: 2 })
+    const expected$ = cold('---c---d--', { c: 1, d: 3 })
+    const ms = 10
 
-        const expected$ = cold('---c---d--', { c: 1, d: 3 });
-
-        expect(main.delayedSum(numbers$, ms, scheduler)).toBeObservable(expected$);
-    })
-});
+    expect(main.delayedSum(numbers$, ms, scheduler)).toBeObservable(expected$)
+  })
+})
 
 describe('#filterChars()', () => {
-    it('should filter chars which are going after param symbol', () => {
-        const values = helpers.generateAlphabet();
+  it('should filter chars which are going after param symbol', () => {
+    const values = helpers.generateAlphabet()
 
-        const chars$ = cold('--a--b--c--d--e--f--', values);
-        const expected$ = cold('-----------d--e--f--', values);
+    const chars$ = cold('--a--b--c--d--e--f--', values)
+    const expected$ = cold('-----------d--e--f--', values)
 
-        expect(main.filterChars(chars$, 'c')).toBeObservable(expected$);
-    })
+    expect(main.filterChars(chars$, 'c')).toBeObservable(expected$)
+  })
 })
 
 describe('#debounceOut()', () => {
-    it('should output only last value', () => {
-        const values = helpers.generateAlphabet();
-        const scheduler = getTestScheduler();
+  it('should output only last value', () => {
+    const scheduler = getTestScheduler()
 
-        const chars$ = cold('--abcd-e', values);
-        const ms = 10;
-        const expected$ = cold('------d-e', values);
+    const events$ =   cold('--abcd-e')
+    const expected$ = cold('------d-e')
+    const ms = 10
 
-        expect(main.debounceOut(chars$, ms, scheduler)).toBeObservable(expected$);
-    });
-});
+    expect(main.debounceOut(events$, ms, scheduler)).toBeObservable(expected$)
+  })
+})
+
+describe('#throttleOut()', () => {
+  it('should output events after time elapsed', () => {
+    const scheduler = getTestScheduler()
+
+    const events$ =   cold('--abcd-e')
+    const expected$ = cold('--a-c--e')
+    const ms = 10
+
+    expect(main.throttleOut(events$, ms, scheduler)).toBeObservable(expected$)
+  })
+})
+
+describe('#searchTerm()', () => {
+  let scheduler: TestScheduler;
+  let ms: number = 10;
+  let termValues: object;
+
+  beforeEach(() => {
+    scheduler = getTestScheduler()
+    termValues = {
+      a: 'te',
+      b: 'ter',
+      c: 'term',
+      d: 'terms'
+    };
+  })
+
+  describe('when term is not correct', () => {
+    it('should not do anything', () => {
+      const event$ =    cold('--a--', { a: '' })
+      const expected$ = cold('-----')
+
+      expect(main.searchTerm(event$, ms, scheduler)).toBeObservable(expected$);
+    })
+  })
+
+  describe('when term is correct', () => {
+
+    describe('when user emits single event', () => {
+      it('should make a request', () => {
+        const term$ =     cold('--a--', termValues)
+        const expected$ = cold('---a-', termValues)
+
+        expect(main.searchTerm(term$, ms, scheduler)).toBeObservable(expected$);
+      })
+    })
+
+    describe('when user emits multiple events', () => {
+      describe('when events are the same', () => {
+        it('should make a single request', () => {
+          const term$ =     cold('--a-a-a-a--a-a-', termValues)
+          const expected$ = cold('---a-----------', termValues)
+
+          expect(main.searchTerm(term$, ms, scheduler)).toBeObservable(expected$);
+        })
+      })
+    })
+
+    describe('when user emits multiple events', () => {
+      describe('when events are different', () => {
+        it('should make a single request', () => {
+          const term$ =     cold('--abc-d---', termValues)
+          const expected$ = cold('-----c-d--', termValues)
+
+          expect(main.searchTerm(term$, ms, scheduler)).toBeObservable(expected$);
+        })
+      })
+    })
+  })
+})
--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,47 @@
-import { Observable, Scheduler, asyncScheduler } from 'rxjs';
-import { scan, delay, filter, debounceTime } from 'rxjs/operators';
+import { Observable, Scheduler, asyncScheduler } from 'rxjs'
+import { scan, delay, filter, debounceTime, throttleTime, distinctUntilChanged } from 'rxjs/operators'
 
-export const sum = (
-    numbers$: Observable<number>
-) => {
-    return numbers$.pipe(
-        scan((total: number, el: number) => total + el, 0)
-    );
+export const sum = (numbers$: Observable<number>) => {
+  return numbers$.pipe(scan((total: number, el: number) => total + el, 0))
 }
 
 export const delayedSum = (
-    input$: Observable<number>,
-    ms: number,
-    scheduler: Scheduler = asyncScheduler
+  input$: Observable<number>,
+  ms: number,
+  scheduler: Scheduler = asyncScheduler
 ) => {
-    return sum(input$).pipe(
-        delay(ms, scheduler)
-    );
+  return sum(input$).pipe(delay(ms, scheduler))
 }
 
-export const filterChars = (
-    chars$: Observable<string>,
-    borderChar: string
-) => {
-    return chars$.pipe(
-        filter((char: string) => char > borderChar)
-    );
+export const filterChars = (chars$: Observable<string>, borderChar: string) => {
+  return chars$.pipe(filter((char: string) => char > borderChar))
 }
 
-export const debounceOut = (
-    events$: Observable<string>,
-    ms: number,
-    scheduler: Scheduler = asyncScheduler
+export const debounceOut = <T>(
+  events$: Observable<T>,
+  ms: number,
+  scheduler: Scheduler = asyncScheduler
 ) => {
-    return events$.pipe(debounceTime(ms, scheduler));
+  return events$.pipe(debounceTime(ms, scheduler))
+}
+
+export const throttleOut = <T>(
+  events$: Observable<T>,
+  ms: number,
+  scheduler: Scheduler = asyncScheduler
+) => {
+  return events$.pipe(throttleTime(ms, scheduler))
+}
+
+
+export const searchTerm = (
+  term$: Observable<string>,
+  ms: number,
+  scheduler: Scheduler = asyncScheduler
+) => {
+  return term$.pipe(
+    filter((term: string) => !!term),
+    distinctUntilChanged(),
+    debounceTime(ms, scheduler)
+  )
 }
