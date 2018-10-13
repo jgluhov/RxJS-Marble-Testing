d960104b9b67dc0e00c3d879dc6c0632fd394f35
Evgeny Glukhov <evglukhov@luxoft.com>
2018-10-13 22:08:47 +0300

    prettier

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,6 +1,6 @@
 import { cold, getTestScheduler } from 'jasmine-marbles'
 import * as main from './main'
-import { TestScheduler } from 'rxjs/testing';
+import { TestScheduler } from 'rxjs/testing'
 
 type Values<V> = {
   [index: string]: V
@@ -13,28 +13,34 @@ const helpers = {
       .map((_: string, i: number) => String.fromCharCode(65 + i).toLowerCase())
   },
   generateAlphabet() {
-    return this.generateSymbols().reduce((acc: Values<string>, char: string) => {
-      acc[char] = char
+    return this.generateSymbols().reduce(
+      (acc: Values<string>, char: string) => {
+        acc[char] = char
 
-      return acc
-    }, {})
+        return acc
+      },
+      {}
+    )
   },
   generateEvents(type: string) {
-    return this.generateSymbols().reduce((acc: Values<object>, char: string) => {
-      acc[char] = {
-        type,
-        key: char,
-        which: char.charCodeAt(0)
-      };
+    return this.generateSymbols().reduce(
+      (acc: Values<object>, char: string) => {
+        acc[char] = {
+          type,
+          key: char,
+          which: char.charCodeAt(0)
+        }
 
-      return acc;
-    }, {})
+        return acc
+      },
+      {}
+    )
   }
 }
 
 describe('#sum()', () => {
   it('should sum numbers correctly', () => {
-    const numbers$ =  cold('--a--b--', { a: 1, b: 2 })
+    const numbers$ = cold('--a--b--', { a: 1, b: 2 })
     const expected$ = cold('--c--d--', { c: 1, d: 3 })
 
     expect(main.sum(numbers$)).toBeObservable(expected$)
@@ -44,7 +50,7 @@ describe('#sum()', () => {
 describe('#delayedSum()', () => {
   it('should sum numbers with delay correctly', () => {
     const scheduler = getTestScheduler()
-    const numbers$ =  cold('--a---b---', { a: 1, b: 2 })
+    const numbers$ = cold('--a---b---', { a: 1, b: 2 })
     const expected$ = cold('---c---d--', { c: 1, d: 3 })
     const ms = 10
 
@@ -67,7 +73,7 @@ describe('#debounceOut()', () => {
   it('should output only last value', () => {
     const scheduler = getTestScheduler()
 
-    const events$ =   cold('--abcd-e')
+    const events$ = cold('--abcd-e')
     const expected$ = cold('------d-e')
     const ms = 10
 
@@ -79,7 +85,7 @@ describe('#throttleOut()', () => {
   it('should output events after time elapsed', () => {
     const scheduler = getTestScheduler()
 
-    const events$ =   cold('--abcd-e')
+    const events$ = cold('--abcd-e')
     const expected$ = cold('--a-c--e')
     const ms = 10
 
@@ -88,9 +94,9 @@ describe('#throttleOut()', () => {
 })
 
 describe('#searchTerm()', () => {
-  let scheduler: TestScheduler;
-  let ms: number = 10;
-  let termValues: object;
+  let scheduler: TestScheduler
+  let ms: number = 10
+  let termValues: object
 
   beforeEach(() => {
     scheduler = getTestScheduler()
@@ -99,36 +105,37 @@ describe('#searchTerm()', () => {
       b: 'ter',
       c: 'term',
       d: 'terms'
-    };
+    }
   })
 
   describe('when term is not correct', () => {
     it('should not do anything', () => {
-      const event$ =    cold('--a--', { a: '' })
+      const event$ = cold('--a--', { a: '' })
       const expected$ = cold('-----')
 
-      expect(main.searchTerm(event$, ms, scheduler)).toBeObservable(expected$);
+      expect(main.searchTerm(event$, ms, scheduler)).toBeObservable(expected$)
     })
   })
 
   describe('when term is correct', () => {
-
     describe('when user emits single event', () => {
       it('should make a request', () => {
-        const term$ =     cold('--a--', termValues)
+        const term$ = cold('--a--', termValues)
         const expected$ = cold('---a-', termValues)
 
-        expect(main.searchTerm(term$, ms, scheduler)).toBeObservable(expected$);
+        expect(main.searchTerm(term$, ms, scheduler)).toBeObservable(expected$)
       })
     })
 
     describe('when user emits multiple events', () => {
       describe('when events are the same', () => {
         it('should make a single request', () => {
-          const term$ =     cold('--a-a-a-a--a-a-', termValues)
+          const term$ = cold('--a-a-a-a--a-a-', termValues)
           const expected$ = cold('---a-----------', termValues)
 
-          expect(main.searchTerm(term$, ms, scheduler)).toBeObservable(expected$);
+          expect(main.searchTerm(term$, ms, scheduler)).toBeObservable(
+            expected$
+          )
         })
       })
     })
@@ -136,10 +143,12 @@ describe('#searchTerm()', () => {
     describe('when user emits multiple events', () => {
       describe('when events are different', () => {
         it('should make a single request', () => {
-          const term$ =     cold('--abc-d---', termValues)
+          const term$ = cold('--abc-d---', termValues)
           const expected$ = cold('-----c-d--', termValues)
 
-          expect(main.searchTerm(term$, ms, scheduler)).toBeObservable(expected$);
+          expect(main.searchTerm(term$, ms, scheduler)).toBeObservable(
+            expected$
+          )
         })
       })
     })
