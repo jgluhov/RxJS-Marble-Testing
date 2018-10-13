21dca505aa275f6997752a167f9917479c7ad21a
Evgeny Glukhov <evglukhov@luxoft.com>
2018-10-13 14:45:08 +0300

    add files and fns

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -2,7 +2,7 @@ import * as main from './main';
 
 describe('#add', () => {
     it('should count two numbers', () => {
-        expect(main.add(5, 5)).toBe(10);
+        
     })
 })
 
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-export const add = (a: number, b: number) => a + b;
+import { Observable } from 'rxjs';
+import { scan } from 'rxjs/operators';
+
+export const sum = (numbers$: Observable<number>) => numbers$
+    .pipe(
+        scan((total: number, el: number) => total + el, 0)
+    );
